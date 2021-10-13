94fb63ecbf82f4de1700b3e27fc4bae767417ef3
Bhargavi-msl <84659197+Bhargavi-msl@users.noreply.github.com>
2021-10-13 10:19:08 +0530

    Update GitDemo.java

--- src/pk1/GitDemo.java
+++ src/pk1/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi! This is Bhargavi!");
-				System.out.println("Hello!");
+				System.out.println("Hola Amigos");
 
 	}
 
